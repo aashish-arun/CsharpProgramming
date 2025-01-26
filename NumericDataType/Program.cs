aa3c161fb6c821efd5340c061d4898ba51ea5dec
aashish-arun <aashish.ouo@gmail.com>
2025-01-25 23:37:34 -0700

    Day-1

---
+++ NumericDataType/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumericDataType
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaring and initializing on different lines
+            int Num; //Declaring a variable
+            Num = 23; //Initializing a value to a variable
+            Console.WriteLine(Num); //Output
+
+
+            //Declaring and initializing on the same line
+            int Num2 = 25;
+            Console.WriteLine(Num2); //Output
+
+
+            //Declaring and initializing on the same line with assigning a different value later on
+            int Num3 = 25;
+            Console.WriteLine(Num3); //Output
+            Num3 = 45;
+            Console.WriteLine(Num3); //Output
+
+
+
+            //Numeric data type
+            int IntNum = 15; //1. For integer numbers - initialize the variable as int and its value should be int32
+            Console.WriteLine(IntNum); //Output
+
+            Console.WriteLine(int.MaxValue); //Max value of int
+            Console.WriteLine(int.MinValue); //Min value of int
+            //Note: int variables can have values which are also negative.
+
+
+            long LongNum = 97969551543523L; //2. For bigger integer numbers - initialize the variable as long and its value should be int64
+            Console.WriteLine(LongNum); //Output
+
+            Console.WriteLine(long.MaxValue); //Max value of long
+            Console.WriteLine(long.MinValue); //Min value of long
+            //Notes: long variables can have values which are also negative. Also the value for a long variable should end with an "L".
+
+
+            float FloatNum = 5F; //3. For floating numbers - initialize the variable as float and its value should be single (i.e. float)
+            Console.WriteLine(FloatNum); //Output
+
+            Console.WriteLine(float.MaxValue); //Max value of float
+            Console.WriteLine(float.MinValue); //Min value of float
+            //Notes: float variables can have values which are also negative. Also the value for a float variable should end with an "F".
+
+
+            double DoubleNum = -55.2D; //4. For bigger floating numbers - initialize the variable as double and its value should also be double
+            Console.WriteLine(DoubleNum); //Output
+
+            Console.WriteLine(double.MaxValue); //Max value of double
+            Console.WriteLine(double.MinValue); //Min value of double
+            //Notes: double variables can have values which are also negative. Also the value for a double variable should end with an "D".
+
+
+            decimal DecimalNum = 55.5M; //5. For decimal numbers - initialize the variable as decimal and its value should also be decimal
+            Console.WriteLine(DecimalNum);
+
+            Console.WriteLine(decimal.MaxValue); //Max value of decimal
+            Console.WriteLine(decimal.MinValue); //Min value of decimal
+            //Notes: decimal variables can have values which are also negative. Also the value for a decimal variable should end with an "M"
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,81 @@
-# CsharpProgramming
+Tracker For "Learn C# Programming – Full Course with Mini-Projects" {
+    [Start Date: 24|01|2025] [Total Time (hr): xx] [Finish Date: DD|MM|2025]
+
+
+    Day-1 {
+        Entry will be done in the Day-2 commit.
+    }
+}
+
+
+Content For "Learn C# Programming – Full Course with Mini-Projects" {
+    (0:00:00) Introduction
+    (0:01:56) Installing Visual Studio 2022
+    (0:05:25) Creating your first project
+    (0:07:48) Hello world
+    (0:10:56) Numeric data types
+    (0:18:08) Text based data types
+    (0:20:32) Converting string to numbers
+    (0:24:27) Boolean data type
+    (0:25:36) Operators
+    (0:31:36) Remainder
+    (0:35:08) Var keyword
+    (0:39:13) Const keyword
+    (0:43:15) Exercise - Storing user data
+    (0:45:05) Exercise - Odd/Even checker
+    (0:46:59) Console Input/Output
+    (0:55:32) If statements
+    (1:09:13) Switch statements
+    (1:14:29) For loops
+    (1:22:28) While loops
+    (1:32:53) Conditional operator
+    (1:36:22) Numeric formatting
+    (1:49:36) TryParse function
+    (1:59:42) Exercise - Times table
+    (2:03:10) Exercise - Fizz buzz game
+    (2:08:24) Verbatim string literal
+    (2:17:00) String formatting
+    (2:22:36) String interpolation
+    (2:25:59) String concatenation
+    (2:30:41) Empty string
+    (2:32:59) String Equals function
+    (2:38:03) String iteration looping
+    (2:47:56) String IsNullOrEmpty function
+    (2:53:07) Exercise - Print string in reverse
+    (3:01:44) Exercise - Password checker
+    (3:11:02) Arrays
+    (3:32:59) Array sorting
+    (3:35:48) Array reversal
+    (3:39:20) Array clearing
+    (3:45:38) Array IndexOf
+    (3:55:00) Lists
+    (4:03:57) Dictionary
+    (4:16:30) Exercise - Odd/Even number split
+    (4:23:11) Exercise - Array of multiples
+    (4:30:00) Functions
+    (4:35:39) Void functions
+    (4:41:32) Return type functions
+    (4:59:48) Function parameters
+    (5:12:24) Optional parameters
+    (5:17:19) Named parameters
+    (5:19:51) Out parameters
+    (5:37:28) Reference parameters
+    (5:46:12) Exercise - Area of a Triangle
+    (5:50:14) Exercise - Sum of int Array
+    (6:01:09) Exception handling
+    (6:03:27) Try…catch
+    (6:12:47) Printing error messages
+    (6:17:31) Exercise - Custom TryParse
+    (6:26:00) Debugging
+    (6:37:55) Local/auto window
+    (6:41:35) Watch window
+    (6:46:40) Exercise - Fix logic error
+    (6:50:14) Structures
+    (7:06:56) Classes
+    (7:15:56) Class functions
+    (7:22:07) Class fields
+    (7:37:27) Class variable/function scope
+    (7:41:30) Class properties
+    (8:01:09) Class ToString function override
+    (8:16:44) Outro
+}
